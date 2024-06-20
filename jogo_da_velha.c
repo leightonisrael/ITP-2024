1229811a633eb783ea9229a52ac09c8ded36fad7
Paulo Rogério <80416920+RealMeloLima@users.noreply.github.com>
2024-06-20 15:02:53 -0300

    Commit jogo da velha
    Fiz umas alterações leves

--- jogo_da_velha.c
+++ jogo_da_velha.c
@@ -4,7 +4,6 @@
 
 #define SIZE 3
 
-// Função para imprimir o tabuleiro
 void printBoard(char *board) {
     for (int i = 0; i < SIZE * SIZE; i++) {
         if (i % SIZE == 0) printf("\n");
@@ -13,32 +12,26 @@ void printBoard(char *board) {
     printf("\n");
 }
 
-// Função para verificar o vencedor
 char checkWinner(char *board) {
-    // Verifica linhas
     for (int i = 0; i < SIZE; i++) {
         if (board[i * SIZE] == board[i * SIZE + 1] && board[i * SIZE + 1] == board[i * SIZE + 2])
             return board[i * SIZE];
     }
-    // Verifica colunas
     for (int i = 0; i < SIZE; i++) {
         if (board[i] == board[SIZE + i] && board[SIZE + i] == board[2 * SIZE + i])
             return board[i];
     }
-    // Verifica diagonais
     if (board[0] == board[4] && board[4] == board[8])
         return board[0];
     if (board[2] == board[4] && board[4] == board[6])
         return board[2];
-    // Verifica se há empate
     for (int i = 0; i < SIZE * SIZE; i++) {
         if (board[i] != 'X' && board[i] != 'O')
-            return '\0'; // Jogo ainda não terminou
+            return '\0';
     }
-    return 'D'; // Empate
+    return 'D';
 }
 
-// Função para mover o jogador
 void playerMove(char *board) {
     int pos;
     printf("Escolha sua posição, jogador X: ");
@@ -51,7 +44,6 @@ void playerMove(char *board) {
     }
 }
 
-// Função para mover o PC
 void computerMove(char *board) {
     int pos;
     srand(time(NULL));
@@ -63,21 +55,18 @@ void computerMove(char *board) {
 }
 
 int main() {
-    // Aloca memória dinamicamente para o tabuleiro
     char *board = (char *)malloc(SIZE * SIZE * sizeof(char));
     if (board == NULL) {
         printf("Falha na alocação de memória.\n");
         return 1;
     }
 
-    // Inicializa o tabuleiro
     for (int i = 0; i < SIZE * SIZE; i++) {
         board[i] = '1' + i;
     }
 
     char winner = '\0';
 
-    // Loop principal do jogo
     while (winner == '\0') {
         printBoard(board);
         playerMove(board);
@@ -87,10 +76,8 @@ int main() {
         winner = checkWinner(board);
     }
 
-    // Imprime o tabuleiro final
     printBoard(board);
 
-    // Declara o resultado
     if (winner == 'X') {
         printf("Parabéns, jogador X, você venceu!\n");
     } else if (winner == 'O') {
@@ -99,7 +86,6 @@ int main() {
         printf("O jogo terminou em empate!\n");
     }
 
-    // Libera a memória alocada
     free(board);
     return 0;
 }
